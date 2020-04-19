370fa124da4f484382a4d9820662ecedeccb3b10
Heber Dias <heberfabiano@gmail.com>
2020-04-19 17:54:09 -0300

    database setup

--- start/routes.ts
+++ start/routes.ts
@@ -19,6 +19,12 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+  return report.healthy ? response.ok(report) : response.badRequest(report)
+})
 
 Route.get('/', async () => {
   return { hello: 'world' }
