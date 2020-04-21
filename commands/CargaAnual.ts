248c1073de5545df19be48d40c7c483253fd7491
Leo Ribeiro <leordev@gmail.com>
2020-04-20 22:56:50 -0400

    adiciona carga de indices

--- commands/CargaAnual.ts
+++ commands/CargaAnual.ts
@@ -29,27 +29,42 @@ interface Instituicao {
   porte: string,
 }
 
+interface Indice {
+  ans_id: number,
+  ano: number,
+  mes: number,
+  classificacao: number,
+  indice: number,
+}
+
+interface InstituicaoIndices {
+  instituicao: Instituicao,
+  indices: Indice[],
+}
+
 interface ObjetoCsv {
   razaoComAns: string,
   cobertura: string,
   porte: string,
-  classificacaoMes: Number,
-  classificacaoMesAnterior: Number,
-  mes12: Number,
-  mes11: Number,
-  mes10: Number,
-  mes09: Number,
-  mes08: Number,
-  mes07: Number,
-  mes06: Number,
-  mes05: Number,
-  mes04: Number,
-  mes03: Number,
-  mes02: Number,
-  mes01: Number,
+  classificacaoMes: string,
+  classificacaoMesAnterior: string,
+  mes12: string,
+  mes11: string,
+  mes10: string,
+  mes9: string,
+  mes8: string,
+  mes7: string,
+  mes6: string,
+  mes5: string,
+  mes4: string,
+  mes3: string,
+  mes2: string,
+  mes1: string,
   competencia: string,
   dataAtualizacao: string, // todo: convert to date
 }
+
+let cargaAnual: string
 
 export default class CargaAnual extends BaseCommand {
   public static commandName = 'carga:anual'
@@ -58,29 +73,45 @@ export default class CargaAnual extends BaseCommand {
   @args.string({ required: true, description: 'Arquivo CSV da Carga' })
   public arquivo: string
 
-  public async handle() {
+  @args.string({ required: false, description: 'Carga Anual passada' })
+  public anual: string
+
+  public async handle () {
+    console.info('this anual', this.anual)
+    cargaAnual = this.anual
+
     const dados = await this.carregaCsv()
     this.logger.info('Linhas', dados.length.toString())
+
     const instituicoes = dados.slice(0, CARGA_MAX_REGISTROS).map(converteInstituicao)
 
     // TODO: inject database
-    const db = knex(DB_CONFIG);
-    let i = 0;
-    let batch: Instituicao[] = [];
-    for (const instituicao of instituicoes) {
-      batch.push(instituicao);
-      i++;
+    const db = knex(DB_CONFIG)
+    let i = 0
+    let batchInstituicoes: Instituicao[] = []
+    let batchIndices: Indice[] = []
+    let inserts = 0
+    for (const instituicaoComIndices of instituicoes) {
+      batchInstituicoes.push(instituicaoComIndices.instituicao)
+      batchIndices = batchIndices.concat(instituicaoComIndices.indices)
+      i++
       if (i % BATCH_SIZE === 0 || i === instituicoes.length) {
-        await db.raw(db.table('instituicoes').insert(batch).toQuery()
-          + ' ON CONFLICT DO NOTHING;');
-        batch = [];
+        const resInstituicoes = await db.raw(db.table('instituicoes').insert(batchInstituicoes).toQuery()
+          + ' ON CONFLICT DO NOTHING RETURNING *;')
+        const resIndices = await db.raw(db.table('indices').insert(batchIndices).toQuery()
+          + ' ON CONFLICT DO NOTHING RETURNING *;')
+        const batchInserts = resInstituicoes.rowCount + resIndices.rowCount
+        inserts += batchInserts
+        this.logger.info(`batch ${i} inserted rows ${batchInserts}`)
+        batchInstituicoes = []
+        batchIndices = []
       }
     }
 
-    this.logger.info('Registros inseridos:', i.toString());
+    this.logger.info(`Registros inseridos: ${inserts}`)
   }
 
-  private async carregaCsv(): Promise<ObjetoCsv[]> {
+  private async carregaCsv (): Promise<ObjetoCsv[]> {
     let filehandle
     let arquivoCsv
     try {
@@ -106,15 +137,15 @@ export default class CargaAnual extends BaseCommand {
       'mes12',
       'mes11',
       'mes10',
-      'mes09',
-      'mes08',
-      'mes07',
-      'mes06',
-      'mes05',
-      'mes04',
-      'mes03',
-      'mes02',
-      'mes01',
+      'mes9',
+      'mes8',
+      'mes7',
+      'mes6',
+      'mes5',
+      'mes4',
+      'mes3',
+      'mes2',
+      'mes1',
       'competencia',
       'dataAtualizacao',
     ]
@@ -131,7 +162,7 @@ export default class CargaAnual extends BaseCommand {
   }
 }
 
-function converteInstituicao(objetoCsv: ObjetoCsv): Instituicao {
+function converteInstituicao (objetoCsv: ObjetoCsv): InstituicaoIndices {
   const { razao, id } = splitRazaoAnsId(objetoCsv.razaoComAns)
 
   const instituicao: Instituicao = {
@@ -139,12 +170,49 @@ function converteInstituicao(objetoCsv: ObjetoCsv): Instituicao {
     nome: razao,
     cobertura: objetoCsv.cobertura,
     porte: objetoCsv.porte,
-  };
+  }
 
-  return (instituicao)
+  const indices: Indice[] = []
+
+  let ano = parseInt(objetoCsv.competencia.substring(0, 4))
+  let mes = parseInt(objetoCsv.competencia.substring(4))
+  if (cargaAnual) {
+    mes = 12
+  }
+
+  for (let i = 12; i > 0; i--) {
+    let classificacaoTxt = i === 12 ? objetoCsv.classificacaoMes
+      : i === 11 ? objetoCsv.classificacaoMesAnterior : '0'
+    let classificacao = parseFloat(classificacaoTxt.replace(',', '.'))
+    if (isNaN(classificacao)) {
+      classificacao = 0
+    }
+
+    let indiceTxt = objetoCsv[`mes${i}`]
+    let indice = parseFloat(indiceTxt.replace(',', '.'))
+    if (isNaN(indice)) {
+      indice = 0
+    }
+
+    indices.push({
+      ans_id: id,
+      ano,
+      mes,
+      classificacao,
+      indice,
+    })
+
+    mes--
+    if (mes === 0) {
+      mes = 12
+      ano--
+    }
+  }
+
+  return ({ instituicao, indices })
 }
 
-function splitRazaoAnsId(valor: string) {
+function splitRazaoAnsId (valor: string) {
   const ansInicio = valor.lastIndexOf('(')
   const razao = valor.substring(0, ansInicio - 1)
   const id = parseInt(valor.substring(ansInicio + 1, valor.lastIndexOf(')')))
--- database/migrations/1587329997644_instituicoes.ts
+++ database/migrations/1587329997644_instituicoes.ts
@@ -3,7 +3,7 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class Instituicoes extends BaseSchema {
   protected tableName = 'instituicoes'
 
-  public async up() {
+  public async up () {
     this.schema.raw(`
       CREATE TABLE ${this.tableName}(
         ans_id INTEGER PRIMARY KEY NOT NULL,
@@ -11,11 +11,11 @@ export default class Instituicoes extends BaseSchema {
         cobertura VARCHAR (255) NOT NULL,
         porte VARCHAR (64) NOT NULL,
         created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-      );`
-    )
+      );
+    `)
   }
 
-  public async down() {
+  public async down () {
     this.schema.dropTable(this.tableName)
   }
 }
