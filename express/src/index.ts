f9f02ce4404dc2b337d03e06ebe7d6781cc8f8e3
Leo Ribeiro <leordev@gmail.com>
2020-04-21 07:00:26 -0400

    setup prettier and lint

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,16 +1,16 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
 const getYear = (date: Date): string => {
-  return `${date.getFullYear()}`
-}
+    return `${date.getFullYear()}`;
+};
 
-app.get('/', (req, res) => {
-  const year = getYear(new Date())
-  res.send(`Hello World ! ${year}`)
-})
+app.get("/", (_req, res) => {
+    const year = getYear(new Date());
+    res.send(`Hello World ! ${year}`);
+});
 
 app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
+    console.log("App listening on port 3000!");
+});
